34bf3ce87003334daa6f48f31e8fc10752b70e12
sebperz <sebperez.inbox@gmail.com>
2026-02-24 14:42:59 -0500

    chessy

--- chess/src/main.rs
+++ chess/src/main.rs
@@ -1,79 +1,63 @@
+use std::io;
+
 fn main() {
-    const LIGHT_SQUARE: &str = "\x1b[48;5;180m";
-    const DARK_SQUARE: &str = "\x1b[48;5;94m";
-    const RESET: &str = "\x1b[0m";
-    const WHITE_PIECE: &str = "\x1b[38;5;255m";
-    const BLACK_PIECE: &str = "\x1b[38;5;0m";
-    const SQUARE_WIDTH: usize = 4;
-
-    let board: [[Option<char>; 8]; 8] = [
-        ['♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖'],
-        ['♙', '♙', '♙', '♙', '♙', '♙', '♙', '♙'],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        ['♟', '♟', '♟', '♟', '♟', '♟', '♟', '♟'],
+    let mut white_movements: String;
+    let mut black_movements: String;
+    let board: [[char; 8]; 8] = [
         ['♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜'],
-    ]
-    .map(|row| row.map(|c| if c == ' ' { None } else { Some(c) }));
+        ['♟', '♟', '♟', '♟', '♟', '♟', '♟', '♟'],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        ['♙', '♙', '♙', '♙', '♙', '♙', '♙', '♙'],
+        ['♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖'],
+    ];
 
-    print!("   ");
-    for c in 'a'..='h' {
-        print!("  {} ", c);
+    // loop {}
+}
+
+fn read_input() -> (char, char) {
+    println!("Type the position of the piece to select it:");
+
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    let mut input = input.trim().chars();
+    let letter = input.next();
+    let number = input.next();
+
+    if input.next().is_some() {
+        println!("Invalid input, type exactly 2 characters");
+        return read_input();
     }
-    println!();
 
-    for row in (0..8).rev() {
-        print!(" {} ", row + 1);
-
-        for col in 0..8 {
-            let is_light = (row + col) % 2 == 0;
-            let bg = if is_light { LIGHT_SQUARE } else { DARK_SQUARE };
-            let piece = board[row][col];
-            let piece_fg = if piece.map(|p| p as u32).unwrap_or(0) > 0x265A {
-                WHITE_PIECE
-            } else {
-                BLACK_PIECE
-            };
-            let content = piece.unwrap_or(' ');
-            print!(
-                "{}{}{:^width$}{}",
-                bg,
-                piece_fg,
-                content,
-                RESET,
-                width = SQUARE_WIDTH
-            );
+    let letter = match letter {
+        Some(c) => c,
+        None => {
+            println!("First Character have to be a letter between a and h");
+            return read_input();
         }
-        println!(" {} ", row + 1);
-
-        print!("   ");
-        for col in 0..8 {
-            let is_light = (row + col) % 2 == 0;
-            let bg = if is_light { LIGHT_SQUARE } else { DARK_SQUARE };
-            let piece = board[row][col];
-            let piece_fg = if piece.map(|p| p as u32).unwrap_or(0) > 0x265A {
-                WHITE_PIECE
-            } else {
-                BLACK_PIECE
-            };
-            let content = piece.unwrap_or(' ');
-            print!(
-                "{}{}{:^width$}{}",
-                bg,
-                piece_fg,
-                content,
-                RESET,
-                width = SQUARE_WIDTH
-            );
+    };
+    let number = match number {
+        Some(c) => c,
+        None => {
+            println!("Second Character have to be a number between 1 and 8");
+            return read_input();
         }
-        println!();
+    };
+
+    if !('a'..='h').contains(&letter) {
+        println!("First Character have to be a letter between a and h");
+        return read_input();
+    }
+    if !('1'..='8').contains(&number) {
+        println!("Second Character have to be a number between 1 and 8");
+        return read_input();
     }
 
-    print!("   ");
-    for c in 'a'..='h' {
-        print!("  {} ", c);
-    }
-    println!();
+    (letter, number)
 }
---
+++ playground/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello, world!");
+
+    let x = 10;
+    let y = 20;
+
+    let max_val = (x > y) ? x : y;
+
+}
+
