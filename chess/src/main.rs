01dae03632d5ec25711b28e9d267f80275a6d94e
sebperz <sebperez.inbox@gmail.com>
2026-03-07 20:02:02 -0500

    working

--- chess/src/main.rs
+++ chess/src/main.rs
@@ -8,59 +8,33 @@ const WHITE_PIECE_COLOR: &str = "\x1B[38;5;231m";
 const BLACK_PIECE_COLOR: &str = "\x1B[38;5;16m";
 const GREEN: &str = "\x1B[48;2;104;157;106m";
 const RESET: &str = "\x1B[0m";
+
 fn main() {
     let mut board: [[char; 8]; 8] = [
-        [' ', ' ', '♜', '♘', '♖', ' ', ' ', ' '], // a1-h1
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], // a2-h2
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', '♚'], // a1-h1
+        [' ', ' ', ' ', ' ', ' ', ' ', '♟', ' '], // a2-h2
         [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', '♘', '♗', '♘'],
+        [' ', ' ', ' ', '♗', ' ', ' ', ' ', ' '],
         [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
         [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], // a7-h7
-        ['♘', ' ', ' ', ' ', ' ', ' ', ' ', ' '], // a8-h8
+        [' ', ' ', ' ', ' ', ' ', ' ', '♟', ' '], // a7-h7
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', '♚'], // a8-h8
     ];
     let mut white_posible_movements: [[bool; 8]; 8] = [[false; 8]; 8];
     let mut black_posible_movements: [[bool; 8]; 8] = [[false; 8]; 8];
     let mut white_pieces_pinned: [[bool; 8]; 8] = [[false; 8]; 8];
     let mut black_pieces_pinned: [[bool; 8]; 8] = [[false; 8]; 8];
 
+    // let mut pinned_pieces: vec ((usize,usize),[[bool;8];8])
     // print_board_pieces(&board);
     print_chess_board(board);
-    // rook_movement(
-    //     (3, 3),
-    //     &board,
-    //     &WHITE_PIECES,
-    //     &BLACK_PIECES,
-    //     &mut white_posible_movements,
-    //     &mut black_pieces_pinned,
-    // );
-    knight_movement(
-        (3, 0),
+    bishop_movement(
+        (3, 3),
         &board,
         &WHITE_PIECES,
         &BLACK_PIECES,
         &mut white_posible_movements,
-    );
-    knight_movement(
-        (5, 3),
-        &board,
-        &WHITE_PIECES,
-        &BLACK_PIECES,
-        &mut white_posible_movements,
-    );
-    knight_movement(
-        (7, 3),
-        &board,
-        &WHITE_PIECES,
-        &BLACK_PIECES,
-        &mut white_posible_movements,
-    );
-    knight_movement(
-        (0, 7),
-        &board,
-        &WHITE_PIECES,
-        &BLACK_PIECES,
-        &mut white_posible_movements,
+        &mut black_pieces_pinned,
     );
     println!("Posible movements:");
     print_posible_movements(white_posible_movements);
@@ -362,6 +336,140 @@ fn rook_movement(
         }
     }
 }
+
+fn bishop_movement(
+    position: (usize, usize),
+    board: &[[char; 8]; 8],
+    ally_pieces: &[char; 6],
+    enemy_pieces: &[char; 6],
+    possible_movements: &mut [[bool; 8]; 8],
+    enemy_piece_pinned: &mut [[bool; 8]; 8],
+) {
+    let (col, row) = position;
+    let right_col = 7 - col;
+    let left_col = col;
+    let top_row = 7 - row;
+    let down_row = row;
+    //maxcol maxrow
+    // maxcol minrow
+    // mincol minrow
+    // mincol maxrow
+    println!("{}", std::cmp::min(right_col, top_row));
+    for iter in 1..=std::cmp::min(right_col, top_row) {
+        let target = board[row + iter][col + iter];
+        if target == ' ' {
+            possible_movements[row + iter][col + iter] = true;
+        } else if ally_pieces.contains(&target) {
+            break;
+        } else {
+            possible_movements[row + iter][col + iter] = true;
+            //Check for pins: if next piece is Black King
+            for jter in std::cmp::min(right_col, top_row) + 1..=7 {
+                if board[row + jter][col + jter] == enemy_pieces[0] {
+                    for i in 1..=7 - jter {
+                        enemy_piece_pinned[row + i][col + i] = true;
+                    }
+                } else if board[row + jter][col + jter] == ' ' {
+                    continue;
+                } else {
+                    break;
+                }
+            }
+            break;
+        }
+    }
+    // for iter_col in col + 1..=7 {
+    //     let target = board[row][iter_col];
+    //     if target == ' ' {
+    //         possible_movements[row][iter_col] = true;
+    //     } else if ally_pieces.contains(&target) {
+    //         break;
+    //     } else {
+    //         possible_movements[row][iter_col] = true;
+    //         //Check for pins: if next piece is Black King
+    //         for jter_col in iter_col + 1..=7 {
+    //             if board[row][jter_col] == enemy_pieces[0] {
+    //                 enemy_piece_pinned[row][col..jter_col].fill(true);
+    //             } else if board[row][jter_col] == ' ' {
+    //                 continue;
+    //             } else {
+    //                 break;
+    //             }
+    //         }
+    //         break;
+    //     }
+    // }
+
+    // for iter_col in (0..col).rev() {
+    //     let target = board[row][iter_col];
+    //     if target == ' ' {
+    //         possible_movements[row][iter_col] = true;
+    //     } else if ally_pieces.contains(&target) {
+    //         break;
+    //     } else {
+    //         possible_movements[row][iter_col] = true;
+    //         //Check for pins: if next piece is Black King
+    //         for jter_col in (0..iter_col).rev() {
+    //             if board[row][jter_col] == enemy_pieces[0] {
+    //                 enemy_piece_pinned[row][jter_col + 1..=col].fill(true);
+    //             } else if board[row][jter_col] == ' ' {
+    //                 continue;
+    //             } else {
+    //                 break;
+    //             }
+    //         }
+    //         break;
+    //     }
+    // }
+
+    // for iter_row in row + 1..=7 {
+    //     let target = board[iter_row][col];
+    //     if target == ' ' {
+    //         possible_movements[iter_row][col] = true;
+    //     } else if ally_pieces.contains(&target) {
+    //         break;
+    //     } else {
+    //         possible_movements[iter_row][col] = true;
+    //         //Check for pins: if next piece is Black King
+    //         for jter_row in iter_row + 1..=7 {
+    //             if board[jter_row][col] == enemy_pieces[0] {
+    //                 for r in row..jter_row {
+    //                     enemy_piece_pinned[r][col] = true;
+    //                 }
+    //             } else if board[jter_row][col] == ' ' {
+    //                 continue;
+    //             } else {
+    //                 break;
+    //             }
+    //         }
+    //         break;
+    //     }
+    // }
+
+    // for iter_row in (0..row).rev() {
+    //     let target = board[iter_row][col];
+    //     if target == ' ' {
+    //         possible_movements[iter_row][col] = true;
+    //     } else if ally_pieces.contains(&target) {
+    //         break;
+    //     } else {
+    //         possible_movements[iter_row][col] = true;
+    //         //Check for pins: if next piece is Black King
+    //         for jter_row in (0..iter_row).rev() {
+    //             if board[jter_row][col] == enemy_pieces[0] {
+    //                 for r in jter_row + 1..=row {
+    //                     enemy_piece_pinned[r][col] = true;
+    //                 }
+    //             } else if board[jter_row][col] == ' ' {
+    //                 continue;
+    //             } else {
+    //                 break;
+    //             }
+    //         }
+    //         break;
+    //     }
+    // }
+}
 fn knight_movement(
     position: (usize, usize),
     board: &[[char; 8]; 8],
@@ -419,6 +527,32 @@ fn knight_movement(
             }
         }
     }
+}
+
+fn queen_movement(
+    position: (usize, usize),
+    board: &[[char; 8]; 8],
+    ally_pieces: &[char; 6],
+    enemy_pieces: &[char; 6],
+    possible_movements: &mut [[bool; 8]; 8],
+    enemy_piece_pinned: &mut [[bool; 8]; 8],
+) {
+    rook_movement(
+        position,
+        board,
+        ally_pieces,
+        enemy_pieces,
+        possible_movements,
+        enemy_piece_pinned,
+    );
+    bishop_movement(
+        position,
+        board,
+        ally_pieces,
+        enemy_pieces,
+        possible_movements,
+        enemy_piece_pinned,
+    );
 }
 
 fn print_posible_movements(board: [[bool; 8]; 8]) {
